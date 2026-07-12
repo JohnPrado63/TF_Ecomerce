0a68e03378555f0e43d46f93ee26d25ad14b727b
stella <stella.sre.inc@gmail.com>
2026-07-12 18:48:23 -0500

    refactor: remove cloud service integrations and update proxy trust configuration

--- ecommerce/config/services.php
+++ ecommerce/config/services.php
@@ -25,11 +25,6 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
     'mercadopago' => [
         'public_key'     => env('MP_PUBLIC_KEY'),
         'access_token'   => env('MP_ACCESS_TOKEN'),
